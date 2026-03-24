bbce5ed8e808b9b71ee695fe3daa46a9981e8352
Brcic, Ramon <ramon.brcic@dlr.de>
2026-03-24 15:53:04 +0100

    added class for global variables

--- src/vicsek_pkg_example/example.py
+++ src/vicsek_pkg_example/example.py
@@ -12,14 +12,18 @@ def test(num):
 
 plt.rcParams['animation.embed_limit'] = 300
 
-n = 200
-d = 0.01
-v = 0.01
-dt = 1
-eta = 0.1
+class Vicsek:
+    def __init__(self, n, d, v, dt, eta):
+        self.n = n # number of objects
+        self.d = d # radius of influence
+        self.v = v # velocity
+        self.dt = dt # time step
+        self.eta = eta # angle
 
-r = np.random.random((n, 2))
-theta = np.random.random(n)
+vicsek = Vicsek(200,0.01,0.01,1,0.1)
+
+r = np.random.random((vicsek.n, 2))
+theta = np.random.random(vicsek.n)
 
 fig, ax = plt.subplots(figsize=(6, 6))
 
@@ -43,14 +47,14 @@ def distance(p1, p2):
 def update_model():
     global r, theta, counter
 
-    for i in range(n):
+    for i in range(vicsek.n):
         sum_sin = 0
         sum_cos = 0
         neighbours = 0
 
-        for j in range(n):
+        for j in range(vicsek.n):
             if i != j:
-                if distance(r[i], r[j]) < d:
+                if distance(r[i], r[j]) < vicsek.d:
                     theta_j = 2 * np.pi * theta[j]
                     sum_sin = sum_sin + np.sin(theta_j)
                     sum_cos = sum_cos + np.cos(theta_j)
@@ -58,10 +62,10 @@ def update_model():
 
         if neighbours > 0:
             avg_theta = np.arctan2(sum_sin / neighbours, sum_cos / neighbours)
-            theta[i] = (avg_theta / (2 * np.pi)) + eta * (np.random.rand() - 0.5)
+            theta[i] = (avg_theta / (2 * np.pi)) + vicsek.eta * (np.random.rand() - 0.5)
 
-        dx = v * dt * np.cos(2 * np.pi * theta[i])
-        dy = v * dt * np.sin(2 * np.pi * theta[i])
+        dx = vicsek.v * vicsek.dt * np.cos(2 * np.pi * theta[i])
+        dy = vicsek.v * vicsek.dt * np.sin(2 * np.pi * theta[i])
 
         r[i, 0] = r[i, 0] + dx
         r[i, 1] = r[i, 1] + dy
@@ -88,7 +92,7 @@ def animate(frame):
     u = []
     vv = []
 
-    for i in range(n):
+    for i in range(vicsek.n):
         x.append(r[i, 0])
         y.append(r[i, 1])
         u.append(np.cos(2 * np.pi * theta[i]))
@@ -101,6 +105,6 @@ def animate(frame):
 
     return q,
 
-test()
+#test()
 ani = FuncAnimation(fig, animate, frames=200, interval=50, blit=True)
 plt.show()
