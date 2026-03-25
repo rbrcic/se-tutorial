5a6a3d934c7b7fb73be5511b5ac9787b3b225646
Brcic, Ramon <ramon.brcic@dlr.de>
2026-03-25 13:37:30 +0100

    fix test distance again

--- test/test_python.py
+++ test/test_python.py
@@ -1,6 +1,9 @@
+import vicsek_pkg_example
+#from vicsek_pkg_example import Vicsek
+
 def test_main():
     assert(True)
 
 def test_distance():
-    assert(Vicsek.distance([0,0],[0,0]) == 0)
-    assert(Vicsek.distance([0,0],[0,1]) == 1)
+    assert(vicsek_pkg_example.Vicsek.distance([0,0],[0,0]) == 0)
+    assert(vicsek_pkg_example.Vicsek.distance([0,0],[0,1]) == 1)
